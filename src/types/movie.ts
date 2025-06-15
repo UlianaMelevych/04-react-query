0258b5195ce11b291505748982653e9524b31f96
UlianaMelevych <uljamelevich17@gmail.com>
2025-06-15 19:39:07 +0300

    movie

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,7 +1,9 @@
 export interface Movie {
   id: number;
-  title: string;
+  poster_path: string;
   backdrop_path: string;
+  title: string;
+  overview: string;
   release_date: string;
   vote_average: number;
 }
