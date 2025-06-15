e5311e6e4b1d8a84cc5ee9ef24b57c0dbfa7f949
UlianaMelevych <uljamelevich17@gmail.com>
2025-06-15 19:31:16 +0300

    movie fixed

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import type { MovieApiResponse } from '../types/movie';
 
-const API_URL = 'https://api.themoviedb.org/3/search/movie';
+const API_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'import.meta.env.VITE_TMDB_TOKEN';
 export const fetchMovies = async (
   query: string,
@@ -18,7 +18,7 @@ export const fetchMovies = async (
         Authorization: `Bearer ${API_KEY}`,
       },
     });
-    console.log('Fetched data:', response.data);
+    
     return response.data;
   } catch (error) {
     console.error('Error fetching movies:', error);
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,22 +1,9 @@
-// export interface Movie {
-//   id: number;
-//   title: string;
-//   poster_path: string;
-//   overview: string;
-// }
-
-// export interface MovieApiResponse {
-//   page: number;
-//   results: Movie[];
-//   total_pages: number;
-//   total_results: number;
-// }
-
 export interface Movie {
   id: number;
   title: string;
-  poster_path: string;
-  overview: string;
+  backdrop_path: string;
+  release_date: string;
+  vote_average: number;
 }
 
 export interface MovieApiResponse {
